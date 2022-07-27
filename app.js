59e7d03b5ee30f131316d71455de171bbfff4f0c
Milen Deyanov <milen.krasimirov.deyanov@gmail.com>
2022-07-27 18:08:33 +0300

    added atlas mongodb

--- app.js
+++ app.js
@@ -11,53 +11,53 @@ const authRoutes = require("./routes/auth");
 
 const User = require('./models/user');
 
-// const MONGODB_URI = 'mongodb://127.0.0.1:27017/Assessment'
+const MONGODB_URI = 'mongodb+srv://7h3blu3:73XBU9fI69AV8jTk@cluster0.dm69koz.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express()
-// const store = new MongoDBStore({
-//   uri: MONGODB_URI,
-//   collection: 'sessions'
-// })
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
 
-// mongoose
-//   .connect(MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(result => {
-//     console.log("Connected to database!");
-//   })
-//   .catch(err => {
-//     console.log(err + " Connection to database failed!");
-//   });
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(result => {
+    console.log("Connected to database!");
+  })
+  .catch(err => {
+    console.log(err + " Connection to database failed!");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(
-//   session({
-//     secret: 'my secret',
-//     resave: false,
-//     saveUninitialized: false,
-//     store: store
-//   })
-// );
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
-// app.use((req, res, next) => {
-//   if (!req.session.user) {
-//     return next();
-//   }
-//   User.findById(req.session.user._id)
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
-// app.use((req, res, next) => {
-//   res.locals.isAuthenticated = req.session.isLoggedIn
-//   next();
-// });
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn
+  next();
+});
 
 
 app.use((req, res, next) => {
